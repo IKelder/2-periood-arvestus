1fda9fbd9a8bf3b3494cec6d77fa86123868a1e6
Ingrid <Kelder>
2014-11-05 11:33:40 +0200

    Ylesanne 3 = valmis

--- ylesanne-3/result.php
+++ ylesanne-3/result.php
@@ -6,6 +6,22 @@
   </head>
 
   <body>
+
+  <?php
+
+$username = isset($_POST["username"]) ? $_POST["username"] : "Puudub";
+$sex = isset($_POST["sex"]) ? $_POST["sex"] : "Puudub";
+if ($username == "Puudub") {
+  echo "Palun minge tagasi ja sisestage nimi.";
+  }
+elseif ($sex == "Puudub") {
+  echo "Palun minge tagasi ja valige sugu";
+} elseif ($username == "Puudub" || $sex == "Puudub") {
+	echo "Palun minge tagasi ja taitke valjad";
+} else {
+	echo "Tere {$username}! Olete {$sex}";
+}
+?>
     <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
     <pre><?php print_r($_POST); ?></pre>
 
